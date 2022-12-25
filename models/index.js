5e01ab250169ac3f7ea760ac5c2e65bfddc18ac5
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-12-25 12:08:10 -0500

    Installed more packages and edited associations in models

--- models/index.js
+++ models/index.js
@@ -1,14 +1,14 @@
 //Import models to connect the associations
 // import all models
-const Post = require("./Post");
-const User = require("./User");
-const Comment = require("./Comment");
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
-  foreignKey: "user_id",
+  foreignKey: 'user_id',
 });
 
 Post.belongsTo(User, {
-  foreignKey: "user_id",
-  onDelete: "SET NULL",
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
 });
